9b4f46bd9a521ef04ff8819fbafa248ac68eb32d
hpuser <hpuser@LAPTOP-N261TKEL>
2017-08-14 19:41:41 -0400

    Added comments to the Driver class

--- BMICalculator/Program.cs
+++ BMICalculator/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*Name: Sahal Mohamed.
+ * Student Number: 300895007.
+ * Date Last Modified: August/15/2017.
+ * Description: This is Assignment 5 of COMP123: BMI Calculator.
+ */
+
 namespace BMICalculator
 {
     public static class Program
